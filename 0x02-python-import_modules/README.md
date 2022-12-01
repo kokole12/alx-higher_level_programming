0ce7ebe05dda6be76e3ed05ea436eb4b7e4edce4
kokole12 <kokoleismail886@gmail.com>
2022-11-30 23:13:14 -0800

    module import

---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,1 @@
+This section is on python import modules which are used to import libraries and files
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add(a, b):
+    """ Addition function
+
+    Args:
+        a: first value
+        b:  second value
+    Returns:
+        the value of a+b
+    """
+    return a + b
