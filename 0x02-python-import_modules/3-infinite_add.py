ad69076386f947864509bb43c8db9db142ca191a
kokole12 <kokoleismail886@gmail.com>
2022-11-30 23:54:26 -0800

    fixing error

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """print all additon arguments"""
+    import sys
+
+    totoal = 0
+    for i in range(len(sys.argv - 1):
+            total += int(sys.argv[i + 1]
+    print("{}".format(total))
