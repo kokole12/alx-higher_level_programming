7a962b072df3fc8009d8eea2c60191f976ea46f4
kokole12 <kokoleismail886@gmail.com>
2022-12-01 10:59:59 +0300

    fixed pycodestyle error

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """print all additon arguments"""
+    """Print the addition of all arguments."""
     import sys
 
-    totoal = 0
-    for i in range(len(sys.argv - 1):
-            total += int(sys.argv[i + 1]
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
     print("{}".format(total))
