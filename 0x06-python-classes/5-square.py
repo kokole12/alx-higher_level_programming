daf1cd13364ec3015fff549e1abbd39f32ff2e7b
kokole12 <kokoleismail886@gmail.com>
2022-12-20 14:36:33 +0300

    print #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+# 2-square.py by Kokole ismail
+""" Declares square"""
+
+
+class Square:
+    """ Initializing the class
+    args:
+        size - initialized to zero
+    Raises:
+        ValueError: if size is less than zero
+        TypeError: if size if not an integer
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """ Retrieves the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Assigns value of size"""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate area of the square
+        Returns: The square of the size
+        """
+
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """ print # the size times"""
+
+        if self.__size == 0:
+            print()
+
+        for i in range(0, self.__size):
+            print("#" * self.__size)
