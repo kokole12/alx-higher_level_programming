a7a19f62572345955789667859b9fcd7cb8b6f45
kokole12 <kokoleismail886@gmail.com>
2022-12-20 13:46:49 +0300

    square size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# 1-sqaure.py by Kokole ismail
+""" Defines a square """
+
+
+class Square:
+    """Representing a square"""
+
+
+    def __init__(self, size):
+        """ Initializing the square class
+        Args: size - represents the size of square
+        """
+
+        
+        self.__size = size
