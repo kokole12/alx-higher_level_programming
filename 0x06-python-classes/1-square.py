a37ce83e32ae8f959c01450ee4f24cc2cfaaba47
kokole12 <kokoleismail886@gmail.com>
2022-12-20 13:49:19 +0300

    fixed pycodestyle error

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,11 +6,9 @@
 class Square:
     """Representing a square"""
 
-
     def __init__(self, size):
         """ Initializing the square class
         Args: size - represents the size of square
         """
 
-        
         self.__size = size
