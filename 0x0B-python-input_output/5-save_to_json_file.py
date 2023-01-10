161d7b18e39e3cf08d93f731bcfda843b40f78a4
kokole Ismail <84512368+kokole12@users.noreply.github.com>
2023-01-10 22:30:34 +0300

    Update 5-save_to_json_file.py
    Fixed

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python
-""" module to  save object as JSON file"""
+#!/usr/bin/python3
+"""This module defines a JSON file-writing function"""
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """ writes an object to a text file using JSON formatting"""
-    with open(filename, 'w') as f:
+    """Writes an object to a text file using JSON format"""
+    with open(filename, "w") as f:
         json.dump(my_obj, f)
