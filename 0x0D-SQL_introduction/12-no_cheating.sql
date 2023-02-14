2493c322cb0b835a68771254eab98c6c666b7c4c
kokole12 <kokoleismail886@gmail.com>
2023-02-14 06:26:28 -0800

    SQL

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- Up[dating record in the table second_table
+UPDATE second_table SET score=10 WHERE name="Bob";
+
---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- Removing records with scores less than 5
+DELETE FROM second_table WHERE score<=5;
+
