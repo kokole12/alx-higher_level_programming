3169ed190e14fb227146c2d73236ee301aa185f8
kokole12 <kokoleismail886@gmail.com>
2023-02-14 06:21:14 -0800

    SQL

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- Listing all scores from second_table
+SELECT score, name FROM second_table ORDER BY score DESC;
+
---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Listing the best score above 10
+SELECT score, name FROM second_table WHERE score>=10 ORDER BY score DESC;
+
