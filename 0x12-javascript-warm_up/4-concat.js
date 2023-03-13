7afa1d9b47a12f162f61d6db9e59fb5769b9f8e4
kokole12 <kokoleismail886@gmail.com>
2023-03-13 13:01:22 +0000

    semeistandard applied

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-arg0 = process.argv[2]
-arg1 = process.argv[3]
-console.log(arg0 + ' is ' + arg1)
-
+const arg0 = process.argv[2];
+const arg1 = process.argv[3];
+console.log(arg0 + ' is ' + arg1);
