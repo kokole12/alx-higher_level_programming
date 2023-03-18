8930a55a909df61331589b6beaaf49755130f619
kokole12 <kokoleismail886@gmail.com>
2023-03-18 12:56:08 +0300

    working on the bug fixes

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -23,4 +23,4 @@ if __name__ == '__main__':
     session = Session()
 
     for instance in session.query(State).order_by(State.id):
-        print('{}:{}'.format(instance.id, instance.name))
+        print('{}: {}'.format(instance.id, instance.name))
