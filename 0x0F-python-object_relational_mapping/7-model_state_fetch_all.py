e03ff90c3445180e8a78995059120e6b95fc9197
kokole12 <kokoleismail886@gmail.com>
2023-03-18 02:26:08 -0700

    pycodestyle

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Script that runs and returns all states
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == '__main__':
+
+    """
+        access to the database to fetch state
+    """
+
+    database_connect = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            argv[1], argv[2], arg[3])
+
+    engine = create_engine(database_connect)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+        print('{}:{}'.format(instance.id, nstance.name)
+
