157e57d8877d18003401ac928fa67ac908730474
kokole12 <kokoleismail886@gmail.com>
2023-03-18 13:04:38 +0300

    fetching first

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    The script runs a query using sqlachemy to return the fist
+    record
+"""
+
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sys import argv
+
+if __name__ == '__main__':
+    """
+        Connecting to the database
+    """
+
+    database_connect = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+
+    engine = create_engine(database_connect)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for instance in session.query_first(State).order_by(State.id):
+        print('{}: {}'.format(instance.id, instance.name))
