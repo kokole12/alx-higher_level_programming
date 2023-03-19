8fc1cb56521c40da631413dc02256909a6aed17a
kokole12 <kokoleismail886@gmail.com>
2023-03-19 12:00:44 +0300

    city state relationship

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
 """
-    Python script to create a state and a city
-    using sqlalchmey
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
 """
 
-from relationship_state import Base, State
 from sys import argv
-from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+from model_city import City
 from sqlalchemy import create_engine
-from relationship_city import City
-
+from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-        Connecting to the database
-        creating object of city
-        creating object of state
-        committing to database
+    Access to the database and get the cities
+    from the database.
     """
 
-    database_connection = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
         argv[1], argv[2], argv[3])
 
-    engine = create_engine(database_connection)
-    Base.metadata.create_all(engine)
+    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    state = State(name='California')
-    city = City(name='San Francisco')
-    state.cities.append(city)
 
+    results = session.query(City, State).join(State)
 
-    session.add(state)
+    for city, state in results.all():
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
+
     session.commit()
     session.close()
+    
