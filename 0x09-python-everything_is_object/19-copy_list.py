0a891807cdb0e2cd659e536991c36909df1787b8
kokole12 <kokoleismail886@gmail.com>
2023-03-22 14:31:05 +0300

    fixed bug on list copy

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""
+    python script to copy and clone and list
+"""
 def copy_list(l):
+    """ Initializing a new list and copying from original list"""
+
     new_l = l.copy()
     return new_l
