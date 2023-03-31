13912fe217900ba6b3ed0df803d344c4f6f1d642
kokol12 <kokoleismail886@gmail.com>
2023-03-31 14:51:03 +0300

    dubbing the code to meet alx requiremet

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
+"""Fetches https://alx-intranet.hbtn.io/status."""
 import urllib.request
-""" Python script to sent request to a given url"""
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(req) as response:
         status = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(status)))
-        print('\t- content: {}'.format(status))
-        print('\t- utf8 content: {}'.format(status.decode('utf-8')))
+        print("Body response:")
+        print("\t- type: {}".format(type(status)))
+        print("\t- content: {}".format(status))
+        print("\t- utf8 content: {}".format(status.decode("utf-8")))
