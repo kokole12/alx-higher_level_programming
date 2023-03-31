7f1e963a72abba14b45cb196c2b82087f62889b9
kokol12 <kokoleismail886@gmail.com>
2023-03-31 16:06:55 +0300

    creating a post request

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Creating a post request with requests"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    res = requests.post(sys.argv[1], data={"email": sys.argv[2]})
