b53c99f8d9712e36adb8cd9ff1f7298e45627f4f
kokol12 <kokoleismail886@gmail.com>
2023-03-31 14:37:00 +0300

    fetching from https://alx-intranet.hbtn.io/status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import urllid.request
+import urllib.request
 """ Python script to sent request to a given url"""
 
 
 if __name__ == "__main__":
-    req = urllid.request.Request('https://alx-intranet.hbtn.io/status')
-    with urllid.request.urlopen(req) as response:
+    req = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
         status = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(status)))
