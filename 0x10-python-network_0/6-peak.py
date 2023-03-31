76b33000750fe0172275bb9480bc099512ed360f
kokol12 <kokoleismail886@gmail.com>
2023-03-31 10:23:41 +0300

    find the peak value in a list

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ fiind the peak value in a list"""
 
+
 def find_peak(list_of_inegers):
     if len(list_of_inegers) == 0:
         return "None"
-    list_of_inegers.sort()
-    return list_of_inegers[-1]
+    list_of_inegers.sort(reversed=True)
+    return list_of_inegers[0]
