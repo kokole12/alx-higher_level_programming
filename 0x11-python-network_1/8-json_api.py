dc485c2c6e9249c73f5ad7aad96a8e1f0061f13e
kokol12 <kokoleismail886@gmail.com>
2023-03-31 16:34:47 +0300

    serching through a payload

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -16,4 +16,4 @@ if __name__ == "__main__":
         else:
             print('[{}] {}'.format(response.get('id'), response.get("name")))
     except ValueError:
-        print('Not a valid Json')
+        print('Not a valid JSON')
