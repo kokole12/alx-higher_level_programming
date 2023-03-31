416a396432313e3d9c1e6e4965d980b1ad5f5eba
kokol12 <kokoleismail886@gmail.com>
2023-03-31 15:11:15 +0300

    retruning the id from headers

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,5 +7,9 @@ import urllib.request
 if __name__ == "__main__":
     req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
-        dict_body = dict(response.headers)
+        headers = response.getheaders()
+        # print(headers)
+        # for header in headers:
+        #     print(header)
+        dict_body = dict(headers)
         print(dict_body.get("X-Request-Id"))
