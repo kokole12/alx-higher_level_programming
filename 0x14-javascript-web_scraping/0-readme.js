49c94faf715286515285b798c74d6979e59b5826
kokole12 <kokoleismail886@gmail.com>
2023-04-25 11:44:01 +0000

    fixing code to requiremente

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const path = require('path');
 const filename = process.argv[2];
-fs.readFile(path.join(__dirname, '/', filename), 'utf-8',
+fs.readFile(filename, 'utf-8',
   (err, data) => {
     if (err) {
       console.log(err);
