96729e6cc4360ec62a768973a098fccd67a254a4
kokol12 <kokoleismail886@gmail.com>
2023-05-03 16:33:19 +0300

    using the getJSON methods to extract from a json file

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,10 +1,17 @@
-const { error } = require('console')
-const request  = require('request')
 const url = "https://swapi-api.alx-tools.com/api/people/5/?format=json"
-
-request.get(url, (error, response, body) => {
-    if(error) {
-        console.log(error);
-    }
-    console.log(body);
+$(document).ready(function() {
+    $.getJSON(url, function(user_details) {
+        $("DIV#character").text(user_details.name);
+    });
 });
+
+// const request  = require('request')
+// const url = "https://swapi-api.alx-tools.com/api/people/5/?format=json"
+
+// request.get(url, (error, response, body) => {
+//     if(error) {
+//         console.log(error);
+//     }
+//     const user_name = JSON.parse(body)
+//     console.log(user_name.name)
+// });
