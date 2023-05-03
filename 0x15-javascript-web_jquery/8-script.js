5106eabaf674928dd27b991a2d6f5b80ba3effc9
kokol12 <kokoleismail886@gmail.com>
2023-05-03 16:55:03 +0300

    movie title

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,8 @@
+const url = "https://swapi-api.alx-tools.com/api/films/?format=json";
+$(document).ready(function() {
+    $.getJSON(url, function(movies){
+        movies.results.forEach(function(movie) {
+            $("<li>").text(movie.title).appendTo("UL#list_movies");
+        });
+    });
+});
