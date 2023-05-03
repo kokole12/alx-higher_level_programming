2f909bf90b6ac7d5aaa9b8dff3208c25f728c46e
kokol12 <kokoleismail886@gmail.com>
2023-05-03 17:52:18 +0300

    greetings in different languages

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,8 +1,8 @@
-const url = "https://swapi-api.alx-tools.com/api/people/5/?format=json"
-$(document).ready(function() {
-    $.getJSON(url, function(user_details) {
-        $("DIV#character").text(user_details.name);
-    });
+const url = 'https://swapi-api.alx-tools.com/api/people/5/?format=json';
+$(document).ready(function () {
+  $.getJSON(url, function (userDetails) {
+    $('DIV#character').text(userDetails.name);
+  });
 });
 
 // const request  = require('request')
--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,6 +1,6 @@
-const url = "https://fourtonfish.com/hellosalut/?lang=fr"
-$(document).ready(function() {
-    $.getJSON(url, function(greeting) {
-        $("DIV#hello").text(greeting.hello);
-    });
+const url = 'https://fourtonfish.com/hellosalut/?lang=fr';
+$(document).ready(function () {
+  $.getJSON(url, function (greeting) {
+    $('DIV#hello').text(greeting.hello);
+  });
 });
