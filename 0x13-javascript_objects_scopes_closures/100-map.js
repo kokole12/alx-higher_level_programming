3a8e7737ab3b7bd438fd65d20db59da8b605be0f
kokole12 <kokoleismail886@gmail.com>
2023-06-20 16:22:13 +0300

    solution to data and map functiopn

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
+
 const elements = require('./100-data').list;
 
 const newArray = elements.map((x, index) => {
   return x * index;
 });
 
+console.log(elements);
 console.log(newArray);
